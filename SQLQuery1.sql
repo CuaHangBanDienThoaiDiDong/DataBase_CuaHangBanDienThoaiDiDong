a15d1796e2275891fc96ed5d20cbc6bbd56b6de8
Gia Thuận <97448995+Thuan17@users.noreply.github.com>
2024-05-03 23:11:12 +0700

    upate truy van

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,6 +11,36 @@ FROM tb_ProductDetail
 WHERE ProductsId = 1
 GROUP BY Capacity ,Color
 
+SELECT MIN(ProductDetailId) AS ProductDetailId, Color
+FROM tb_ProductDetail
+WHERE ProductsId = 1
+GROUP BY Color
+
+SELECT MIN(ProductDetailId) AS ProductDetailId, Capacity
+FROM tb_ProductDetail
+WHERE ProductsId = 1
+GROUP BY Capacity
+
+
+SELECT MIN(ProductDetailId) AS ProductDetailId, Color AS FilteredValue1
+FROM tb_ProductDetail
+WHERE ProductsId = 3
+GROUP BY Color
+
+UNION
+
+SELECT MIN(ProductDetailId) AS ProductDetailId, CAST(Capacity AS NVARCHAR(MAX)) AS FilteredValue
+FROM tb_ProductDetail
+WHERE ProductsId = 3
+GROUP BY Capacity
+
+
+select Price , PriceSale from tb_ProductDetail where Color='Xám'and Capacity ='256' and ProductsId = 3
+
+select * from tb_ProductDetail where Color='Titan xanh'and Capacity ='1000' and ProductsId = 1
+
+
+
 
 
 SELECT MIN(ProductDetailId) AS ProductDetailId,Capacity
