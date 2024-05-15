c2cc9c874a25554b3635aa3d483d3e6da7adef4a
Gia Thuận <97448995+Thuan17@users.noreply.github.com>
2024-05-15 23:45:34 +0700

    add data

--- DB_CUAHANGDIENTHOAI.sql
+++ DB_CUAHANGDIENTHOAI.sql
@@ -380,6 +380,28 @@ create table tb_ReviewDetail (
 )
 go
 
+create table Provinces(
+	idProvinces int identity(1,1) primary key,
+	name nvarchar(255),
+);
+go
+
+create table Districts(
+	idDistricts int identity(1,1) primary key,
+	name nvarchar(255),
+	idProvinces int,
+	foreign key (idProvinces) references Provinces(idProvinces)
+);
+go
+
+create table Wards(
+	idWards int identity(1,1) primary key,
+	name nvarchar(255),
+	idDistricts int,
+	foreign key (idDistricts) references Districts(idDistricts)
+);
+go
+
 
 -------------------------------------------------------------------------TRIGGER
 
